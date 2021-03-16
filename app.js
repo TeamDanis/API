967043e5ba4e19028f242e8b362b31820f1d9b04
dcalper00 <dcalper00@gmail.com>
2021-03-16 17:34:51 +0100

    arreglar heroku

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const md5 = require('md5');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000
 
 //conexion mongo
 var mongo = require('mongodb').MongoClient;
